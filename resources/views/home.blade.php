d4cc8a5c2b7a3feebc57ddc4db6a800f58bef1f4
ZiyadK147 <Ziyadkhan2333@gmail.com>
2024-04-02 16:44:29 +0500

    basic theme applied sidebar and some thing remains

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.layout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+    <div class="row mt-4 justify-content-center">
+
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -19,5 +19,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
